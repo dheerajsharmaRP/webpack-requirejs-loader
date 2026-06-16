e96bce80263a975df97ccf6e8e8309bec886f347
Dheeraj <dheerajsharma@rightpoint.com>
2026-06-17 04:38:43 +0530

    Update RequireJsLoaderPlugin for webpack 5 compatibility

--- RequireJsLoaderPlugin.js
+++ RequireJsLoaderPlugin.js
@@ -1,17 +1,18 @@
 'use strict';
 
-const ConcatSource = require('webpack-sources').ConcatSource;
+const { ConcatSource } = require('webpack-sources');
 
 let RequireJsLoaderPlugin = function () {
 };
 
+function isNormalModule(module) {
+    return Boolean(module && typeof module.request === 'string' && module.rawRequest != null);
+}
+
 function gatherRequireJsImports(modules) {
     let needsImport = [];
     for (let module of modules) {
-        // If the requirejs-loader was used, then we need to wrap and import this module.
-        // It's safe to use mixins! in all cases, and necessary for anything where require('mixins').hasMixins(module) is true.
-        // TODO: Clean up this check.
-        if (module.request && module.request.indexOf('requirejs-loader') !== -1) {
+        if (isNormalModule(module) && String(module.request).indexOf('requirejs-loader') !== -1) {
             needsImport.push('mixins!' + module.rawRequest);
         }
     }
@@ -28,32 +29,65 @@ function generateEpilog(imports) {
     return `});`;
 }
 
-function registerHook(object, oldName, newName, cb) {
-    if (object.hooks) {
-        object.hooks[newName].tap('RequireJsLoader', cb);
-    } else {
-        object.plugin(oldName, cb);
-    }
-}
-
 RequireJsLoaderPlugin.prototype.apply = function (compiler) {
-    registerHook(compiler, 'compilation', 'compilation', (compilation, data) => {
-        registerHook(compilation, 'chunk-asset', 'chunkAsset', (chunk, filename) => {
-            // Avoid applying imports twice.
-            if ('--requirejs-export:done' in chunk) {
-                return;
-            }
+    const isWebpack5 = Boolean(compiler.webpack);
 
-            const modules = chunk.modulesIterable ? Array.from(chunk.modulesIterable) : modules;
-            const needsImport = gatherRequireJsImports(modules);
-            if (needsImport.length != 0) {
-                let prolog = generateProlog(needsImport);
-                let epilog = generateEpilog(needsImport);
+    if (isWebpack5) {
+        compiler.hooks.compilation.tap('RequireJsLoaderPlugin', (compilation) => {
+            const Compilation = compiler.webpack.Compilation;
 
-                compilation.assets[filename] = new ConcatSource(prolog, "\n", compilation.assets[filename], "\n", epilog);
-            }
+            compilation.hooks.processAssets.tap(
+                {
+                    name: 'RequireJsLoaderPlugin',
+                    stage: Compilation.PROCESS_ASSETS_STAGE_SUMMARIZE,
+                },
+                () => {
+                    for (const chunk of compilation.chunks) {
+                        const chunkModules = Array.from(
+                            compilation.chunkGraph.getChunkModulesIterable(chunk)
+                        );
+
+                        const needsImport = gatherRequireJsImports(chunkModules);
+
+                        if (needsImport.length === 0) continue;
+
+                        const prolog = generateProlog(needsImport);
+                        const epilog = generateEpilog(needsImport);
+
+                        for (const filename of chunk.files) {
+                            if (!filename.endsWith('.js')) continue;
+
+                            compilation.updateAsset(
+                                filename,
+                                (old) => new ConcatSource(prolog, '\n', old, '\n', epilog)
+                            );
+                        }
+                    }
+                }
+            );
         });
-    });
+    } else {
+        compiler.hooks.compilation.tap('RequireJsLoaderPlugin', (compilation) => {
+            compilation.hooks.chunkAsset.tap('RequireJsLoaderPlugin', (chunk, filename) => {
+                if (!filename.endsWith('.js')) return;
+
+                // Avoid applying imports twice.
+                if ('--requirejs-export:done' in chunk) {
+                    return;
+                }
+
+                const modules = chunk.modulesIterable ? Array.from(chunk.modulesIterable) : [];
+                const needsImport = gatherRequireJsImports(modules);
+                
+                if (needsImport.length !== 0) {
+                    let prolog = generateProlog(needsImport);
+                    let epilog = generateEpilog(needsImport);
+
+                    compilation.assets[filename] = new ConcatSource(prolog, "\n", compilation.assets[filename], "\n", epilog);
+                }
+            });
+        });
+    }
 };
 
 module.exports = RequireJsLoaderPlugin;
